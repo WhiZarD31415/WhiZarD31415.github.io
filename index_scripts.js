2cd0e05ea792c28798876ba2247316711ea738f9
Connor Vitz <connorvitz@gmail.com>
2024-11-01 16:59:47 -0400

    2nd attempt to fix exports

--- index_scripts.js
+++ index_scripts.js
@@ -1,13 +1,11 @@
-import WelcomePage from "./classes/Welcome/WelcomePage";
+import WelcomePage from "./classes/Welcome/WelcomePage.js";
 
 const welcomePage = new WelcomePage("This is a welcome!");
 welcomePage.render();
 
-/* 
 document.getElementById('updateButton').addEventListener('click', () => {
     const newContent = prompt("Enter new content:");
     if (newContent) {
-        myPage.updateContent(newContent);
+        welcomePage.updateContent(newContent);
     }
 });
- */

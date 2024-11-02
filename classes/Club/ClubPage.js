1a5d8e79c7847ec050b18984f2f9b3d932bd44f7
Connor Vitz <connorvitz@gmail.com>
2024-11-02 18:37:42 -0400

    Adding different Pages

---
+++ classes/Club/ClubPage.js
@@ -0,0 +1,14 @@
+import Page from "../Page.js";
+
+class ClubPage extends Page {
+    constructor(content) {
+        super(content);
+    }
+
+    render() {
+        const contentDiv = document.getElementById('content');
+        contentDiv.innerHTML = this.content;
+    }
+}
+
+export default ClubPage;
